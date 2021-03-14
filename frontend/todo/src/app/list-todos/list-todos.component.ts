95501d4462ef0fc7d106f1f9724912018d891282
Darin <darin.brown.re@gmail.com>
2021-03-14 09:19:18 -0700

    pre bootstrapping

--- frontend/todo/src/app/list-todos/list-todos.component.ts
+++ frontend/todo/src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ) {
+  }
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -7,14 +17,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListTodosComponent implements OnInit {
   todos = [
-    {id: 1, description : 'Assemble bike shoes'},
-    {id: 2, description: 'Become Angular Expert'},
-    {id: 3, description: 'Become Spring Boot Expert'},
-    {id: 4, description: 'Become cloud hosting expert'},
-    {id: 5, description: 'Update personal website'},
-    {id: 6, description: 'Create website for Jason'},
-    {id: 7, description: 'Meet with Tate'},
-    {id: 8, description: 'Create Compentency Map / Justification / Goals'}
+    new Todo (1,  'Assemble bike shoes', false, new Date('3/31/2021')),
+    new Todo ( 2,'Become Angular Expert', false, new Date('3/31/2021')),
+    new Todo (3,  'Become Spring Boot Expert', false, new Date('3/31/2021')),
+    new Todo (4,  'Become cloud hosting expert', false, new Date('3/31/2021')),
+    new Todo (5, 'Update personal website', false, new Date('3/31/2021')),
+    new Todo (6, 'Create website for Jason', false, new Date('3/31/2021')),
+    new Todo (7, 'Meet with Tate', false, new Date('3/31/2021')),
+    new Todo (8, 'Create Compentency Map / Justification / Goals', false, new Date('3/31/2021'))
   ]
   // todo = {
   //   id : 1,
